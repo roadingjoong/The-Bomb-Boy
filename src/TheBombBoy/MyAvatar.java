4f8a07e894133a662ac1a6776ce4e1f1c7a3e124
kimgwonhyeok <kimgh8182@naver.com>
2024-01-09 22:21:37 +0900

    refactor: Move inner class MyAvatar from InGame.java to MyAvatar.java

---
+++ src/TheBombBoy/MyAvatar.java
@@ -0,0 +1,571 @@
+package TheBombBoy;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+public class MyAvatar {
+	
+	public Image makeAvatar1() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 40, 30, 20);
+		g.fillOval(50, 40, 30, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(30, 45, 10, 10);
+		g.fillOval(60, 45, 10, 10);
+		
+		g.setColor(Color.RED);
+		g.fillOval(40, 70, 20, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar1_up() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar1_right() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(70, 40, 30, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(85, 45, 10, 10);
+		
+		g.setColor(Color.RED);
+		g.fillOval(70, 70, 20, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar1_left() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(0, 40, 30, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(5, 45, 10, 10);
+		
+		g.setColor(Color.RED);
+		g.fillOval(10, 70, 20, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar2() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.BLUE);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 40, 30, 20);
+		g.fillOval(50, 40, 30, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(30, 45, 10, 10);
+		g.fillOval(60, 45, 10, 10);
+		
+		g.setColor(Color.RED);
+		g.fillOval(40, 70, 20, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar2_up() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.BLUE);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar2_right() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.BLUE);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(70, 40, 30, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(85, 45, 10, 10);
+		
+		g.setColor(Color.RED);
+		g.fillOval(70, 70, 20, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar2_left() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.BLUE);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(0, 40, 30, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(5, 45, 10, 10);
+		
+		g.setColor(Color.RED);
+		g.fillOval(10, 70, 20, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar3() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.RED);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 40, 30, 20);
+		g.fillOval(50, 40, 30, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(30, 45, 10, 10);
+		g.fillOval(60, 45, 10, 10);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(40, 70, 20, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar3_up() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.RED);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar3_right() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.RED);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(70, 40, 30, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(85, 45, 10, 10);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(70, 70, 20, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar3_left() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.RED);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(0, 40, 30, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(5, 45, 10, 10);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(10, 70, 20, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar4() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.GREEN);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 40, 30, 20);
+		g.fillOval(50, 40, 30, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(30, 45, 10, 10);
+		g.fillOval(60, 45, 10, 10);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(40, 70, 20, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar4_up() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.GREEN);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar4_right() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.GREEN);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(70, 40, 30, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(85, 45, 10, 10);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(70, 70, 20, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar4_left() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.GREEN);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(0, 40, 30, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(5, 45, 10, 10);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(10, 70, 20, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar5() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.MAGENTA);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 40, 30, 20);
+		g.fillOval(50, 40, 30, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(30, 45, 10, 10);
+		g.fillOval(60, 45, 10, 10);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(40, 70, 20, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar5_up() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.MAGENTA);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar5_right() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.MAGENTA);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(70, 40, 30, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(85, 45, 10, 10);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(70, 70, 20, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar5_left() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.MAGENTA);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(20, 9, 25, 20);
+		g.fillOval(10, 25, 10, 10);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(0, 40, 30, 20);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(5, 45, 10, 10);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(10, 70, 20, 10);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+	public Image makeAvatar_die() {
+		
+		Image newAvatar = new BufferedImage(100, 110, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newAvatar.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(5, 85, 90, 20);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(0, 0, 100, 100);
+		
+		g.dispose();
+		
+		return newAvatar;
+	}
+	
+}
