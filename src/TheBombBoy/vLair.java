f2cfd23e03f474719dbe0ce2c31259e93e5d724a
kimgwonhyeok <kimgh8182@naver.com>
2024-01-09 22:30:01 +0900

    refactor: Move inner class vLair from InGame.java to vLair.java

---
+++ src/TheBombBoy/vLair.java
@@ -0,0 +1,527 @@
+package TheBombBoy;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+public class vLair {
+	
+	public Image makeLair1() {
+		
+		Image newLair = new BufferedImage(200,250, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newLair.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(25, 210, 150, 30);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(0, 0, 200, 200);
+		
+		g.setColor(Color.RED);
+		g.fillOval(20, 60, 40, 40);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 20, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(100, 20, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 40, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(160, 60, 5, 5);
+		
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(30, 130, 5, 5);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 150, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(80, 160, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(120, 140, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 90, 40, 40);
+		
+		g.dispose();
+		
+		return newLair;
+	}
+	
+	public Image makeLair2() {
+		
+		Image newLair = new BufferedImage(200,250, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newLair.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(25, 210, 150, 30);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(0, 0, 200, 200);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(20, 60, 40, 40);
+		
+		g.setColor(Color.RED);
+		g.fillOval(50, 20, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(100, 20, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 40, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(160, 60, 5, 5);
+		
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(30, 130, 5, 5);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 150, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(80, 160, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(120, 140, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 90, 40, 40);
+		
+		g.dispose();
+		
+		return newLair;
+	}
+	
+	public Image makeLair3() {
+		
+		Image newLair = new BufferedImage(200,250, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newLair.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(25, 210, 150, 30);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(0, 0, 200, 200);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(20, 60, 40, 40);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 20, 30, 30);
+		
+		g.setColor(Color.RED);
+		g.fillOval(100, 20, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 40, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(160, 60, 5, 5);
+		
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(30, 130, 5, 5);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 150, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(80, 160, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(120, 140, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 90, 40, 40);
+		
+		g.dispose();
+		
+		return newLair;
+	}
+	
+	public Image makeLair4() {
+		
+		Image newLair = new BufferedImage(200,250, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newLair.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(25, 210, 150, 30);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(0, 0, 200, 200);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(20, 60, 40, 40);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 20, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(100, 20, 20, 20);
+		
+		g.setColor(Color.RED);
+		g.fillOval(140, 40, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(160, 60, 5, 5);
+		
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(30, 130, 5, 5);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 150, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(80, 160, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(120, 140, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 90, 40, 40);
+		
+		g.dispose();
+		
+		return newLair;
+	}
+	
+	public Image makeLair5() {
+		
+		Image newLair = new BufferedImage(200,250, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newLair.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(25, 210, 150, 30);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(0, 0, 200, 200);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(20, 60, 40, 40);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 20, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(100, 20, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 40, 10, 10);
+		
+		g.setColor(Color.RED);
+		g.fillOval(160, 60, 5, 5);
+		
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(30, 130, 5, 5);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 150, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(80, 160, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(120, 140, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 90, 40, 40);
+		
+		g.dispose();
+		
+		return newLair;
+	}
+	
+	public Image makeLair6() {
+		
+		Image newLair = new BufferedImage(200,250, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newLair.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(25, 210, 150, 30);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(0, 0, 200, 200);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(20, 60, 40, 40);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 20, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(100, 20, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 40, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(160, 60, 5, 5);
+		
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(30, 130, 5, 5);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 150, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(80, 160, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(120, 140, 30, 30);
+		
+		g.setColor(Color.RED);
+		g.fillOval(140, 90, 40, 40);
+		
+		g.dispose();
+		
+		return newLair;
+	}
+	
+	public Image makeLair7() {
+		
+		Image newLair = new BufferedImage(200,250, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newLair.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(25, 210, 150, 30);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(0, 0, 200, 200);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(20, 60, 40, 40);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 20, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(100, 20, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 40, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(160, 60, 5, 5);
+		
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(30, 130, 5, 5);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 150, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(80, 160, 20, 20);
+		
+		g.setColor(Color.RED);
+		g.fillOval(120, 140, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 90, 40, 40);
+		
+		g.dispose();
+		
+		return newLair;
+	}
+	
+	public Image makeLair8() {
+		
+		Image newLair = new BufferedImage(200,250, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newLair.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(25, 210, 150, 30);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(0, 0, 200, 200);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(20, 60, 40, 40);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 20, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(100, 20, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 40, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(160, 60, 5, 5);
+		
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(30, 130, 5, 5);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 150, 10, 10);
+		
+		g.setColor(Color.RED);
+		g.fillOval(80, 160, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(120, 140, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 90, 40, 40);
+		
+		g.dispose();
+		
+		return newLair;
+	}
+	
+	public Image makeLair9() {
+		
+		Image newLair = new BufferedImage(200,250, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newLair.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(25, 210, 150, 30);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(0, 0, 200, 200);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(20, 60, 40, 40);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 20, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(100, 20, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 40, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(160, 60, 5, 5);
+		
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(30, 130, 5, 5);
+		
+		g.setColor(Color.RED);
+		g.fillOval(50, 150, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(80, 160, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(120, 140, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 90, 40, 40);
+		
+		g.dispose();
+		
+		return newLair;
+	}
+	
+	public Image makeLair10() {
+		
+		Image newLair = new BufferedImage(200,250, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newLair.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(25, 210, 150, 30);
+		
+		g.setColor(Color.BLACK);
+		g.fillOval(0, 0, 200, 200);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(20, 60, 40, 40);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 20, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(100, 20, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 40, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(160, 60, 5, 5);
+		
+		
+		g.setColor(Color.RED);
+		g.fillOval(30, 130, 5, 5);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 150, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(80, 160, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(120, 140, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 90, 40, 40);
+		
+		g.dispose();
+		
+		return newLair;
+	}
+	
+	public Image makeLair_damage() {
+		
+		Image newLair = new BufferedImage(200,250, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = newLair.getGraphics();
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(25, 210, 150, 30);
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(0, 0, 200, 200);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(20, 60, 40, 40);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 20, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(100, 20, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 40, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(160, 60, 5, 5);
+		
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(30, 130, 5, 5);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(50, 150, 10, 10);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(80, 160, 20, 20);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(120, 140, 30, 30);
+		
+		g.setColor(Color.GRAY);
+		g.fillOval(140, 90, 40, 40);
+		
+		g.dispose();
+		
+		return newLair;
+	}
+	
+}
